32247cc15adc617798d1bf833a1937ee67d0d047
srgjo27 <96277339+srgjo27@users.noreply.github.com>
2024-01-09 13:18:02 +0700

    push project front end

---
+++ Home.js
@@ -0,0 +1,38 @@
+// Accordio
+const accordionItemHeaders = document.querySelectorAll(".accordion-item-header");
+
+accordionItemHeaders.forEach(accordionItemHeader => {
+  accordionItemHeader.addEventListener("click", event => {
+    accordionItemHeader.classList.toggle("active");
+    const accordionItemBody = accordionItemHeader.nextElementSibling;
+    if(accordionItemHeader.classList.contains("active")) {
+      accordionItemBody.style.maxHeight = accordionItemBody.scrollHeight + "px";
+    }
+    else {
+      accordionItemBody.style.maxHeight = 0;
+    }
+  });
+});
+
+//Card flip
+$(".flip-card").click(function(){
+  $(this).toggleClass("card-rotate");
+});
+
+//Popup Card
+$(function() {
+	//Open
+	$('[data-popup-open]').on('click', function(e) {
+		var targeted_popup_class = jQuery(this).attr('data-popup-open');
+		$('[data-popup="' + targeted_popup_class + '"]').fadeIn(350);
+
+		e.preventDefault();
+	});
+  //Close
+	$('[data-popup-close]').on('click', function(e) {
+		var targeted_popup_class = jQuery(this).attr('data-popup-close');
+		$('[data-popup="' + targeted_popup_class + '"]').fadeOut(350);
+
+		e.preventDefault();
+	});
+})
